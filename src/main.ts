a07264e4132fba2c2a6e863d6af7d7906a48acf9
ilhammramadhan <ilhamram20@gmail.com>
2023-06-19 11:32:55 +0700

    setting port

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
+const PORT = process.env.PORT || 3000
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -27,7 +28,7 @@ async function bootstrap() {
     ],
   });
 
-  await app.listen(3000);
+  await app.listen(PORT);
 }
 bootstrap();
  
